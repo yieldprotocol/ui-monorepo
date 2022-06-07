1a1d76ce6b5d880df83bc49575c6887b2fcac3c4
marcomariscal <marco.a.mariscal@gmail.com>
2022-06-07 14:04:15 -0400

    feat: type optimizations

--- packages/core/src/observables/provider.ts
+++ packages/core/src/observables/provider.ts
@@ -1,12 +1,12 @@
-import { ethers } from "ethers";
-import { Observable, BehaviorSubject, share } from "rxjs";
-import defaultProvider from "../config/defaultprovider";
+import { ethers } from 'ethers';
+import { Observable, BehaviorSubject, share } from 'rxjs';
+import defaultProvider from '../config/defaultprovider';
 
 /** @internal */
-export const provider$: BehaviorSubject<ethers.providers.BaseProvider> = new BehaviorSubject(defaultProvider);
+export const provider$ = new BehaviorSubject<ethers.providers.BaseProvider>(defaultProvider);
 // export const provider$: Subject<ethers.providers.BaseProvider> = new Subject();
 
-export const providerø: Observable<ethers.providers.BaseProvider> = provider$.pipe(share());
+export const providerø = provider$.pipe<ethers.providers.BaseProvider>(share());
 export const updateProvider = (newProvider: ethers.providers.BaseProvider) => {
   provider$.next(newProvider); // update to whole new protocol
 };
