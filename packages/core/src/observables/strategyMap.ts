729b694f9920f65d605b565f699d5bf2115ed3af
marcomariscal <marco.a.mariscal@gmail.com>
2022-06-07 14:29:31 -0400

    feat: type optis

--- packages/core/src/observables/strategyMap.ts
+++ packages/core/src/observables/strategyMap.ts
@@ -1,24 +1,24 @@
-import { BehaviorSubject, Observable, share, combineLatest, withLatestFrom, filter } from "rxjs";
+import { BehaviorSubject, Observable, share, combineLatest, withLatestFrom, filter } from 'rxjs';
 import { BigNumber, ethers } from 'ethers';
 import { mulDecimal, divDecimal } from '@yield-protocol/ui-math';
 
 import * as contracts from '../contracts';
-import { ISeries, IStrategy, IStrategyRoot, IYieldProtocol, MessageType } from "../types";
+import { ISeries, IStrategy, IStrategyRoot, IYieldProtocol, MessageType } from '../types';
 
-import { account$ } from "./account";
-import { provider$ } from "./provider";
-import { yieldProtocol$ } from "./yieldProtocol";
-import { seriesMap$ } from "./seriesMap";
-import { ZERO_BN } from "../utils/constants";
-import { sendMsg } from "./messages";
+import { account$ } from './account';
+import { provider$ } from './provider';
+import { yieldProtocol$ } from './yieldProtocol';
+import { seriesMap$ } from './seriesMap';
+import { ZERO_BN } from '../utils/constants';
+import { sendMsg } from './messages';
 
 /** @internal */
-export const strategyMap$: BehaviorSubject<Map<string, IStrategy>> = new BehaviorSubject(new Map([]));
-export const strategyMapø: Observable<Map<string, IStrategy>> = strategyMap$.pipe(share());
+export const strategyMap$ = new BehaviorSubject<Map<string, IStrategy>>(new Map([]));
+export const strategyMapø = strategyMap$.pipe<Map<string, IStrategy>>(share());
 
 /* Update strategies function */
 export const updateStrategies = async (strategyList?: IStrategy[]) => {
-  const list = strategyList?.length ? strategyList : Array.from(strategyMap$.value.values()); 
+  const list = strategyList?.length ? strategyList : Array.from(strategyMap$.value.values());
   list.map(async (_strategy: IStrategy) => {
     const strategyUpdate = await _updateStrategy(_strategy, seriesMap$.value, account$.value);
     strategyMap$.next(new Map(strategyMap$.value.set(_strategy.id, strategyUpdate))); // note: new Map to enforce ref update
@@ -28,18 +28,16 @@ export const updateStrategies = async (strategyList?: IStrategy[]) => {
 /* Observe YieldProtocol$ changes, and update map accordingly */
 yieldProtocol$
   .pipe(
-    filter((protocol )=> protocol.strategyRootMap.size > 0 ),
+    filter((protocol) => protocol.strategyRootMap.size > 0),
     withLatestFrom(provider$)
-    )
-  .subscribe(async ([_protocol, _provider]: [IYieldProtocol, ethers.providers.BaseProvider]) => {
+  )
+  .subscribe(async ([_protocol, _provider]) => {
     /* 'Charge' all the assets (using the current provider) */
-    const chargedList = Array.from(_protocol.strategyRootMap.values()).map((st: IStrategyRoot) =>
-      _chargeStrategy(st, _provider)
-    );
+    const chargedList = Array.from(_protocol.strategyRootMap.values()).map((st) => _chargeStrategy(st, _provider));
     /* Update the assets with dynamic/user data */
     await updateStrategies(chargedList);
-    sendMsg({message:'Strategies Loaded.', type: MessageType.INTERNAL})
-    sendMsg({message: 'Protocol Ready...', type: MessageType.INTERNAL, id: 'protocolLoaded' })
+    sendMsg({ message: 'Strategies Loaded.', type: MessageType.INTERNAL });
+    sendMsg({ message: 'Protocol Ready...', type: MessageType.INTERNAL, id: 'protocolLoaded' });
   });
 
 /* Observe provider$ changes, and update map accordingly ('charge assets/series' with live contracts & listeners ) */
@@ -50,14 +48,12 @@ yieldProtocol$
 // })
 
 /* Observe Account$ changes ('update dynamic/User Data') */
-account$
-.pipe(withLatestFrom(strategyMap$))
-.subscribe( ([account ]) => {
-  console.log( 'account changed:', account )
-})
+account$.pipe(withLatestFrom(strategyMap$)).subscribe(([account]) => {
+  console.log('account changed:', account);
+});
 
 /* Add on extra/calculated Strategy info, contract instances and methods (no async calls) */
-const _chargeStrategy = (strategy: any, provider: ethers.providers.BaseProvider) : IStrategy => {
+const _chargeStrategy = (strategy: any, provider: ethers.providers.BaseProvider): IStrategy => {
   const _strategy = contracts.Strategy__factory.connect(strategy.address, provider);
   return {
     ...strategy,
@@ -68,7 +64,7 @@ const _chargeStrategy = (strategy: any, provider: ethers.providers.BaseProvider)
 
 const _updateStrategy = async (
   strategy: IStrategy,
-  seriesMap: Map<string,ISeries>,
+  seriesMap: Map<string, ISeries>,
   account?: string | undefined
 ): Promise<IStrategy> => {
   /* Dynamic strategy info ( not related to a user ) */
@@ -102,10 +98,7 @@ const _updateStrategy = async (
       strategy.strategyContract.balanceOf(account),
       currentSeries?.poolContract.balanceOf(account),
     ]);
-    const accountStrategyPercent = mulDecimal(
-      divDecimal(accountBalance, strategyTotalSupply || '0'),
-      '100'
-    );
+    const accountStrategyPercent = mulDecimal(divDecimal(accountBalance, strategyTotalSupply || '0'), '100');
     accountData = {
       ...strategy,
       accountBalance,
